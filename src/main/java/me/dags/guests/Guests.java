e00f749e7b080a06215b93608b06e5746a3db59c
dags- <dags@dags.me>
2016-09-05 13:09:36 +0100

    Allow guests to interact with doors but not trap-doors

--- src/main/java/me/dags/guests/Guests.java
+++ src/main/java/me/dags/guests/Guests.java
@@ -1,27 +1,18 @@
 package me.dags.guests;
 
+import org.spongepowered.api.block.BlockSnapshot;
 import org.spongepowered.api.data.key.Keys;
-import org.spongepowered.api.entity.EntitySnapshot;
-import org.spongepowered.api.entity.EntityTypes;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Cancellable;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.Order;
 import org.spongepowered.api.event.block.ChangeBlockEvent;
 import org.spongepowered.api.event.block.InteractBlockEvent;
-import org.spongepowered.api.event.cause.entity.spawn.EntitySpawnCause;
-import org.spongepowered.api.event.cause.entity.spawn.SpawnType;
-import org.spongepowered.api.event.cause.entity.spawn.SpawnTypes;
-import org.spongepowered.api.event.entity.DamageEntityEvent;
 import org.spongepowered.api.event.entity.InteractEntityEvent;
 import org.spongepowered.api.event.entity.SpawnEntityEvent;
-import org.spongepowered.api.event.filter.cause.First;
 import org.spongepowered.api.event.filter.cause.Root;
 import org.spongepowered.api.event.game.state.GameInitializationEvent;
 import org.spongepowered.api.event.item.inventory.ChangeInventoryEvent;
-import org.spongepowered.api.event.item.inventory.ClickInventoryEvent;
-import org.spongepowered.api.event.item.inventory.CreativeInventoryEvent;
-import org.spongepowered.api.event.item.inventory.DropItemEvent;
 import org.spongepowered.api.event.world.LoadWorldEvent;
 import org.spongepowered.api.plugin.Plugin;
 
@@ -88,7 +79,8 @@ public class Guests
     @Listener (order = Order.LAST)
     public void onBlockInteractSec(InteractBlockEvent.Secondary event, @Root Player player)
     {
-        if (event.getTargetBlock().supports(Keys.OPEN) && player.hasPermission(interactOpenable))
+        BlockSnapshot block = event.getTargetBlock();
+        if (block.supports(Keys.HINGE_POSITION) && block.supports(Keys.OPEN) && player.hasPermission(interactOpenable))
         {
             return;
         }
